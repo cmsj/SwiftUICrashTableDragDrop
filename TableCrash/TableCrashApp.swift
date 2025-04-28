31aff0515ff85fd1e384b1c1c0d47295c7c28aec
Chris Jones <cmsj@tenshu.net>
2025-04-28 22:59:42 +0100

    Initial Commit

---
+++ TableCrash/TableCrashApp.swift
@@ -0,0 +1,17 @@
+//
+//  TableCrashApp.swift
+//  TableCrash
+//
+//  Created by Chris Jones on 28/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct TableCrashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
